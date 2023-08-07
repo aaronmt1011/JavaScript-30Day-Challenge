1e6f2b2120192409902e106069ab73df6b031b4d
aaronmt1011 <aaronmt1011@gmail.com>
2023-08-07 15:48:24 -0600

    Finished populateList function

--- 15-Local_Storage/script.js
+++ 15-Local_Storage/script.js
@@ -14,7 +14,7 @@ function addItem(e) {
     will refer to the inputted object. .value is used to get the user inputted 
     value.
     */
-    const text = (this.querySelector('[name = item]')).value;
+    const text = (this.querySelector('[name=item]')).value;
     
     /*
     item obj will be used to place inputted items into (similar to a template)
@@ -23,17 +23,61 @@ function addItem(e) {
         // ES6 is used below to shorten - text: 'text' - to just 'text'.
         text,
         done: false
-    }
+    };
+
+
+    // use .push to push new item objs into items array.
+    items.push(item);
+
+    // use populateList and passes through items and itemsList for parameters
+    populateList(items, itemsList);
+
     /*
     'this' is a form element which means it has various functions attached to it.
     .reset() is used to clear the text from the input boxes. It's similar to a
     reset/restart button that is on various forms to just restart the form again.
     */
-    
-    items.push(item);
     this.reset();
-    console.log(item);
 }
+
+
+/*
+Function populateList() is going to place newly created items on a the list
+above the add item button.
+plates is used in place of the items array from above to work with any scenario.
+platesList is used as a new element for the items to be added too.
+
+** This type of function may come in handy in the future!!! **
+
+*/
+function populateList(plates = [], platesList) {
+
+
+    /*
+    The following can be broken down into a couple of steps:
+        1) Loops through every object in the plates array using map
+        2) return a nice message
+        3) use join('') to make array become a long string because .map() 
+            returns an array which isn't necessary because innerHTML 
+            will return strings, not arrays. 
+    */
+    platesList.innerHTML = plates.map((plate, i) => {
+        /*
+        used data-index=${i} to link the input and the label together.
+        cannot use check="false" because it will still check everything. Having
+        any attribute of check will cause it to be checked. Have to use ternary
+        operator to get through this.
+        */
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
+                <label for="item${i}">${plate.text}</label>
+            </li>
+        `;
+    }).join('');
+}
+
+
 
 /* 
 Adding a 'submit' instead of a 'click' will enable the .addEventListener to
